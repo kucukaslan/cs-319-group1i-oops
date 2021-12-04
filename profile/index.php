692ac40cedfb0ec9cbd060b486415ab67de003fe
Giray Akyol <giray.akyol@ug.bilkent.edu.tr>
2021-12-04 17:21:54 +0300

    templated main page

--- profile/index.php
+++ profile/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Profile Page of <?php echo $_SESSION['cname']?></title>
+    <title>Profile Page of <?php echo $_SESSION['cname'] ?></title>
     <link rel="stylesheet" href="../styles.css">
 
     <meta name="author" content="Muhammed Can Küçükaslan">
@@ -13,30 +13,33 @@
 <div class=\"container\">
 
     <?php
+    require_once '../vendor/autoload.php';
+
+
     include "../config.php";
     session_start();
     $conn = getDatabaseConnection();
 
-    if(! isset($_SESSION['id'] )) {
-        echo"<div class='centerwrapper'> <div class = 'centerdiv'>"
+    if (!isset($_SESSION['id'])) {
+        echo "<div class='centerwrapper'> <div class = 'centerdiv'>"
             . "You haven't logged in";
         echo "<form method='get' action=\"..\"><div class=\"form-group\">
                         <input type=\"submit\" class=\"button button_submit\" value=\"Go to Login Page\">
                     </div> </form>";
         echo "</div> </div>";
-    }
-    else {
+    } else {
+        $m = new Mustache_Engine(array(
+            'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/../templates'),
+        ));
+        echo $m->render('navbar', ['links' => [
+                ['href' => '../', 'title' => 'Main Menu'],
+                ['href' => '../events', 'title' => 'Events'],
+                ['href' => '../closecontact', 'title' => 'Close Contact'],
+                ['href' => '../profile', 'title' => 'Profile'],
+                ['href' => '../logout.php', 'title' => 'Logout', 'id' => 'logout']]]
+        );
 
-
-        echo '<header class="navbar">
-        <a class="navbar-text" background_color="blue" href="..">Main Menu</a>
-        <a class="navbar-text" href="../closecontact">Close Contact</a>
-        <a class="navbar-text" href="../events">Events</a>
-        <a class="navbar-text" href="../profile" >Profile</a>
-        <a class="navbar-text" href="../logout.php" id="logout" >Logout</a>
-        </header class="navbar">';
-
-        echo "<h3> <abbr title='Your Majesties, Your Excellencies, Your Highnesses'>Hey</abbr> ". $_SESSION['firstname']." </h3>";
+        echo "<h3> <abbr title='Your Majesties, Your Excellencies, Your Highnesses'>Hey</abbr> " . $_SESSION['firstname'] . " </h3>";
         echo "<i> Welcome to the <abbr title='arguably'>smallest</abbr> ... University Contact Tracing Service, <abbr title='of course by us'> <b>ever</b></abbr>!</i>";
 
     }
