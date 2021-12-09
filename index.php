e2b45e247c7411dbcfcb2f4b0a31a2b985b64863
Hikmet Simsir <hikmet.simsir@ug.bilkent.edu.tr>
2021-12-09 21:51:39 +0300

    Profile and vax component have added. src folder is added for source files and an example default picture is added for test purposes

--- index.php
+++ index.php
@@ -22,7 +22,9 @@
 </header>
 -->
 <div class="container">
-    <?php
+    <p id='info'>
+
+        <?php
       
         $conn = getDatabaseConnection();
 
@@ -31,18 +33,18 @@
         } else {
             $id = $_SESSION['id'];
             require_once './vendor/autoload.php';
-            $navbar = new Mustache_Engine(array(
-                'loader' => new Mustache_Loader_FilesystemLoader(rootDirectory() . '/templates'),
+            $engine = new Mustache_Engine(array(
+                'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/templates'),
             ));
-            echo $navbar->render('navbar', ['links' => [
+            echo $engine->render('navbar', ['links' => [
                     ['href' => './', 'title' => 'Main Menu'],
                     ['href' => './events', 'title' => 'Events'],
                     ['href' => './closecontact', 'title' => 'Close Contact'],
                     ['href' => './profile', 'title' => 'Profile'],
                     ['href' => './logout.php', 'title' => 'Logout', 'id' => 'logout']]]
             );
-            $welcome = new Mustache_Engine(array(
-                'loader' => new Mustache_Loader_FilesystemLoader(rootDirectory(). '/templates'),
+            /*$welcome = new Mustache_Engine(array(
+                'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/templates'),
             ));
             echo $welcome->render('welcome', ['firstname' => $_SESSION['firstname'], 'lastname' => $_SESSION['lastname']]
             );
@@ -53,7 +55,22 @@
                     ], 'bot' => ' ... University Contact Tracing Service, ',
                     'bm' => ['top' => 'dwdw', 'bm' => false]]
 
+            );*/
+
+            $engine = new Mustache_Engine(array(
+                'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/templates'),
+            ));
+
+            // render and print profile component
+            echo $engine->render("profile", ["name"=>"Feridun", "email"=>"email@emailoglu", "id" => 23232, "allowance"=>"Allowed"]);
+
+            // vaccine component
+            echo $engine->render("vax", ['vaccine' => [
+                    ['vaccineDate' => 'bugun', 'vaccineType' => 'TURKOVAC'],
+                    ['vaccineDate' => 'yarin', 'vaccineType' => 'TURKOVAC']]]
             );
+
+
             /*
                                  ['text' => 'Welcome to the', 'i' => true],
                     ['text' => 'smallest', 'i' => true, 'b' => true],
@@ -63,14 +80,22 @@
         }
 
         ?>
+<!--    </p>
     <div class="centerwrapper">
         <div class="centerdiv">
             <br><br>
             <h2>Some Titles/Tables in the Main Menu</h2>
-            
+
             <br>
         </div>
     </div>
+
+    <form method='post' action="./profile">
+        <div class="form-group">
+            <input type="submit" class="button button_submit" value="Go to Profile Page">
+        </div>
+    </form>
+!-->
 </div>
 </body>
 </html>
