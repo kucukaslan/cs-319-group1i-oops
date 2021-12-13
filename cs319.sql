54d635792052baf8236b439d0365a59bb14d52fe
MUHAMMED CAN KÜÇÜKASLAN <muhammedcankucukaslan@gmail.com>
2021-12-13 21:27:30 +0300

    Update Back-up

--- cs319.sql
+++ cs319.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 12, 2021 at 11:17 PM
+-- Generation Time: Dec 13, 2021 at 07:26 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.11
 
@@ -24,6 +24,43 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Stand-in structure for view `academic_staff`
+-- (See below for the actual view)
+--
+DROP VIEW IF EXISTS `academic_staff`;
+CREATE TABLE `academic_staff` (
+`id` int(11)
+,`registration_date` timestamp
+,`index_id` int(11)
+,`name` varchar(255)
+,`lastname` varchar(255)
+,`email` varchar(255)
+,`password_hash` varchar(255)
+,`hescode` char(10)
+,`profile_picture` blob
+);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `sports_center_staff`
+-- (See below for the actual view)
+--
+DROP VIEW IF EXISTS `sports_center_staff`;
+CREATE TABLE `sports_center_staff` (
+`id` int(11)
+,`index_id` int(11)
+,`name` varchar(255)
+,`lastname` varchar(255)
+,`email` varchar(255)
+,`password_hash` varchar(255)
+,`hescode` char(10)
+,`profile_picture` blob
+);
+
+-- --------------------------------------------------------
+
+--
 -- Stand-in structure for view `student`
 -- (See below for the actual view)
 --
@@ -31,6 +68,24 @@ DROP VIEW IF EXISTS `student`;
 CREATE TABLE `student` (
 `id` int(11)
 ,`registration_date` timestamp
+,`index_id` int(11)
+,`name` varchar(255)
+,`lastname` varchar(255)
+,`email` varchar(255)
+,`password_hash` varchar(255)
+,`hescode` char(10)
+,`profile_picture` blob
+);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `university_administration`
+-- (See below for the actual view)
+--
+DROP VIEW IF EXISTS `university_administration`;
+CREATE TABLE `university_administration` (
+`id` int(11)
 ,`index_id` int(11)
 ,`name` varchar(255)
 ,`lastname` varchar(255)
@@ -67,9 +122,51 @@ INSERT INTO `user` (`index_id`, `id`, `name`, `lastname`, `email`, `password_has
 (2, 2, 'Mustafa', 'K', 'a@b', '$2a$10$j7fjSm.dNIIo7ovzBEIU7udL.IHKWl2X2ydCVm/cJHhyE50np9kw2', NULL, NULL),
 (3, 3, 'Mustafa', 'K', 'a@c', '$2a$10$j7fjSm.dNIIo7ovzBEIU7udL.IHKWl2X2ydCVm/cJHhyE50np9kw2', NULL, NULL),
 (5, 5, 'Testing', 'lol', 'a@e', '$2a$10$j7fjSm.dNIIo7ovzBEIU7udL.IHKWl2X2ydCVm/cJHhyE50np9kw2', '1234567890', NULL),
-(6, 123, '123', '123', '123@123', '123', NULL, NULL),
-(9, 11, 'Register', 'Tester', 'q@w', '123', NULL, NULL),
+(6, 123, '123', '123', '123@123', '$argon2i$v=19$m=65536,t=4,p=1$T0FxL05xcGlac2p0cnNEYQ$i3nCwT70kdtT4wlZJCLfU06fBu0rK8m78Yzj3cAxTO8', NULL, NULL),
+(9, 11, 'Register', 'Tester', 'q@w', '$argon2i$v=19$m=65536,t=4,p=1$T0FxL05xcGlac2p0cnNEYQ$i3nCwT70kdtT4wlZJCLfU06fBu0rK8m78Yzj3cAxTO8', NULL, NULL),
 (10, 907, 'utku', 'jjjj', 'gg@ggsdjsıg.vom', '$argon2i$v=19$m=65536,t=4,p=1$T0FxL05xcGlac2p0cnNEYQ$i3nCwT70kdtT4wlZJCLfU06fBu0rK8m78Yzj3cAxTO8', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_academic_staff`
+--
+
+DROP TABLE IF EXISTS `user_academic_staff`;
+CREATE TABLE `user_academic_staff` (
+  `id` int(11) NOT NULL,
+  `registration_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_turkish_ci;
+
+--
+-- Dumping data for table `user_academic_staff`
+--
+
+INSERT INTO `user_academic_staff` (`id`, `registration_date`) VALUES
+(1, '2021-12-09 14:13:40'),
+(2, '2021-12-09 14:28:10'),
+(3, '2021-12-09 14:28:10'),
+(5, '2021-12-09 14:28:10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_sports_center_staff`
+--
+
+DROP TABLE IF EXISTS `user_sports_center_staff`;
+CREATE TABLE `user_sports_center_staff` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_sports_center_staff`
+--
+
+INSERT INTO `user_sports_center_staff` (`id`) VALUES
+(1),
+(2),
+(3);
 
 -- --------------------------------------------------------
 
@@ -88,14 +185,50 @@ CREATE TABLE `user_student` (
 --
 
 INSERT INTO `user_student` (`id`, `registration_date`) VALUES
-(0, '2021-12-09 14:13:40'),
 (1, '2021-12-09 14:13:40'),
 (2, '2021-12-09 14:28:10'),
 (3, '2021-12-09 14:28:10'),
-(4, '2021-12-09 14:28:10'),
-(5, '2021-12-09 14:28:10'),
-(11, '2021-12-11 22:25:09'),
-(907, '2021-12-11 22:28:26');
+(5, '2021-12-09 14:28:10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_university_administration`
+--
+
+DROP TABLE IF EXISTS `user_university_administration`;
+CREATE TABLE `user_university_administration` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_university_administration`
+--
+
+INSERT INTO `user_university_administration` (`id`) VALUES
+(1),
+(2),
+(3);
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `academic_staff`
+--
+DROP TABLE IF EXISTS `academic_staff`;
+
+DROP VIEW IF EXISTS `academic_staff`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`cs319-1I`@`%` SQL SECURITY DEFINER VIEW `academic_staff`  AS SELECT `user_academic_staff`.`id` AS `id`, `user_academic_staff`.`registration_date` AS `registration_date`, `user`.`index_id` AS `index_id`, `user`.`name` AS `name`, `user`.`lastname` AS `lastname`, `user`.`email` AS `email`, `user`.`password_hash` AS `password_hash`, `user`.`hescode` AS `hescode`, `user`.`profile_picture` AS `profile_picture` FROM (`user_academic_staff` join `user` on(`user_academic_staff`.`id` = `user`.`id`)) ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `sports_center_staff`
+--
+DROP TABLE IF EXISTS `sports_center_staff`;
+
+DROP VIEW IF EXISTS `sports_center_staff`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`cs319-1I`@`%` SQL SECURITY DEFINER VIEW `sports_center_staff`  AS SELECT `user`.`id` AS `id`, `user`.`index_id` AS `index_id`, `user`.`name` AS `name`, `user`.`lastname` AS `lastname`, `user`.`email` AS `email`, `user`.`password_hash` AS `password_hash`, `user`.`hescode` AS `hescode`, `user`.`profile_picture` AS `profile_picture` FROM (`user` join `user_sports_center_staff` on(`user`.`id` = `user_sports_center_staff`.`id`)) ;
 
 -- --------------------------------------------------------
 
@@ -106,6 +239,16 @@ DROP TABLE IF EXISTS `student`;
 
 DROP VIEW IF EXISTS `student`;
 CREATE ALGORITHM=UNDEFINED DEFINER=`cs319-1I`@`%` SQL SECURITY DEFINER VIEW `student`  AS SELECT `user_student`.`id` AS `id`, `user_student`.`registration_date` AS `registration_date`, `user`.`index_id` AS `index_id`, `user`.`name` AS `name`, `user`.`lastname` AS `lastname`, `user`.`email` AS `email`, `user`.`password_hash` AS `password_hash`, `user`.`hescode` AS `hescode`, `user`.`profile_picture` AS `profile_picture` FROM (`user_student` join `user` on(`user_student`.`id` = `user`.`id`)) ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `university_administration`
+--
+DROP TABLE IF EXISTS `university_administration`;
+
+DROP VIEW IF EXISTS `university_administration`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`cs319-1I`@`%` SQL SECURITY DEFINER VIEW `university_administration`  AS SELECT `user_university_administration`.`id` AS `id`, `user`.`index_id` AS `index_id`, `user`.`name` AS `name`, `user`.`lastname` AS `lastname`, `user`.`email` AS `email`, `user`.`password_hash` AS `password_hash`, `user`.`hescode` AS `hescode`, `user`.`profile_picture` AS `profile_picture` FROM (`user_university_administration` join `user` on(`user_university_administration`.`id` = `user`.`id`)) ;
 
 --
 -- Indexes for dumped tables
@@ -122,11 +265,30 @@ ALTER TABLE `user`
   ADD UNIQUE KEY `hescode` (`hescode`);
 
 --
+-- Indexes for table `user_academic_staff`
+--
+ALTER TABLE `user_academic_staff`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `student_id_uid` (`id`) USING BTREE;
+
+--
+-- Indexes for table `user_sports_center_staff`
+--
+ALTER TABLE `user_sports_center_staff`
+  ADD UNIQUE KEY `sport_staff_id_uindex` (`id`);
+
+--
 -- Indexes for table `user_student`
 --
 ALTER TABLE `user_student`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `student_id_uid` (`id`) USING BTREE;
+
+--
+-- Indexes for table `user_university_administration`
+--
+ALTER TABLE `user_university_administration`
+  ADD UNIQUE KEY `instructor_id_uindex` (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -137,6 +299,34 @@ ALTER TABLE `user_student`
 --
 ALTER TABLE `user`
   MODIFY `index_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user_academic_staff`
+--
+ALTER TABLE `user_academic_staff`
+  ADD CONSTRAINT `fk_uas_user` FOREIGN KEY (`id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user_sports_center_staff`
+--
+ALTER TABLE `user_sports_center_staff`
+  ADD CONSTRAINT `fk_scs_user` FOREIGN KEY (`id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user_student`
+--
+ALTER TABLE `user_student`
+  ADD CONSTRAINT `fk_us_user` FOREIGN KEY (`id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user_university_administration`
+--
+ALTER TABLE `user_university_administration`
+  ADD CONSTRAINT `fk_uua_user` FOREIGN KEY (`id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
