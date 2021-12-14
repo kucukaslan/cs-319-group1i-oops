01e4a03d7d4055df85995b16ee6068349ce91e40
Giray Akyol <giray.akyol@ug.bilkent.edu.tr>
2021-12-15 00:38:56 +0300

    preliminary verison, works on my side

--- events/index.php
+++ events/index.php
@@ -8,7 +8,7 @@ startDefaultSessionWith();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>{{title}}</title>
+    <title>Events</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
     <style>
