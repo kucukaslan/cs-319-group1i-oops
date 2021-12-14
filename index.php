d14762d6114afd5fe7a79cca19258dc020acc555
Giray Akyol <giray.akyol@ug.bilkent.edu.tr>
2021-12-14 23:25:41 +0300

    preliminary verison, works on my side

--- index.php
+++ index.php
@@ -21,95 +21,147 @@ startDefaultSessionWith();
 </head>
 <body>
 
+<?php
+$conn = getDatabaseConnection();
 
-<div class="container">
-    <p id="info">
-        <?php
-        $conn = getDatabaseConnection();
 
-        
-        if (!isset($_SESSION['id']) || !isset($conn)) {
-            header("location: ./login");
-        } else {
-            $id = $_SESSION['id'];
-            $uf = new UserFactory(Student::TABLE_NAME);
+if (!isset($_SESSION['id']) || !isset($conn)) {
+    header("location: ./login");
+} else {
+    $id = $_SESSION['id'];
+    $uf = new UserFactory(Student::TABLE_NAME);
+    $std = $uf->makeUserById($conn, $_SESSION['id']);
+
+    //----
+    if (isset($conn) && $_SERVER["REQUEST_METHOD"] == "POST") {
+        if (isset($_POST['HESCode'])) {
+
+            $hescode = $_POST['HESCode'];
+            $std->updateHESCode($hescode);
             $std = $uf->makeUserById($conn, $_SESSION['id']);
-    
-            //----
-            if (isset($conn) && $_SERVER["REQUEST_METHOD"] == "POST") {
-                if (isset($_POST['HESCode'])) {
+        }  // userid and password sent from the form
 
-                    $hescode = $_POST['HESCode'];
-                    $std->updateHESCode($hescode);
-                    $std = $uf->makeUserById($conn, $_SESSION['id']);
-                }  // userid and password sent from the form
-
-            }
-
-            $navbar = new NavBar(Student::TABLE_NAME);
-            echo $navbar->draw();
-            $engine = new Mustache_Engine(array(
-                'loader' => new Mustache_Loader_FilesystemLoader(rootDirectory() . '/templates'),
-
-            ));
-            echo $engine->render('welcome', ['firstname' => $std->getFirstName(), 'lastname' => $std->getLastName()]);
+    }
 
 
-            // render and print profile component sessiondan al name,email falan.
-            echo $engine->render("profile", ["name" => $std->getFirstName(), "email" => $std->getEmail(), "id" => $std->getId(),
-                "allowance" => "Allowed", 'hescode' => $std->getHESCode()]);
+    $navbar = new NavBar(Student::TABLE_NAME);
+    echo $navbar->draw();
+
+}
+?>
+<div class="block"></div>
+<div class="tile is-ancestor notification is-primary">
+    <div class="tile is-parent is-4">
+        <div class="tile is-child box">
+            <p class="title">
+                <?php
+                $engine = new Mustache_Engine(array(
+                    'loader' => new Mustache_Loader_FilesystemLoader(rootDirectory() . '/templates'),
+
+                ));
+                // render and print profile component sessiondan al name,email falan.
+                echo $engine->render("profile", ["name" => $std->getFirstName(), "email" => $std->getEmail(), "id" => $std->getId(),
+                    "allowance" => "Allowed", 'hescode' => $std->getHESCode()]);
+                /*
+                echo $engine->render('welcome', ['firstname' => $std->getFirstName(), 'lastname' => $std->getLastName()]);
+                */
 
 
-            // vaccine component
-            echo $engine->render('vax',
-                ['vaccine' => [
-                    ['vaccineDate' => 'bugun', 'vaccineType' => 'TURKOVAC'],
-                    ['vaccineDate' => 'yarin', 'vaccineType' => 'TURKOVAC']
-                ]
-                ]);
-
-            $pastTest = ["date" => "1.2.4.5", "result" => "negative"];
-            $upcomingTest = ["date" => "2023"];
-
-            // upcoming test are loaded first so that they appear on the top of the table.
-            echo $engine->render("PCRtest", ["upcomingTest" => [
-                $upcomingTest, $upcomingTest
-            ], "pastTest" => [
-                $pastTest, $pastTest
-            ]]);
-
-            echo $engine->render("diagnosis", ["diagnosis" => [
-                ["date" => "date 1"],
-                ["date" => "date 2"]]]);
-        }
-
-
-        ?>
-
-        <!-- delete account button !-->
-    <form method='post' action="./delete">
-        <div class="form-group">
-            <input type="submit" href="/delete" class="button button_delete" value="!!! DELETE ACCOUNT !!!">
+                ?>
+            </p>
         </div>
-    </form>
-    </p>
+    </div>
+    <div class="tile is-parent is-vertical">
+        <div class="tile is-child box">
+            <div class="container">
+                <?php
+                // vaccine component
+                echo $engine->render('vax',
+                    ['vaccine' => [
+                        ['vaccineDate' => 'bugun', 'vaccineType' => 'TURKOVAC'],
+                        ['vaccineDate' => 'yarin', 'vaccineType' => 'TURKOVAC']
+                    ]
+                    ]);
+
+                ?>
+            </div>
+        </div>
+        <div class="tile is-child box">
+            <p>
+                <?php
+                echo $engine->render("diagnosis",
+                    ["diagnosis" => [
+                        ["date" => "date 1"],
+                        ["date" => "date 2"]
+                    ]]);
+                ?>
+            </p>
+        </div>
+    </div>
+    <div class="tile is-parent">
+        <div class="tile is-child box">
+            <p>
+                <?php
+                $pastTest = ["date" => "1.2.4.5", "result" => "negative"];
+                $upcomingTest = ["date" => "2023"];
+
+                // upcoming test are loaded first so that they appear on the top of the table.
+                echo $engine->render("PCRtest", ["upcomingTest" => [
+                    $upcomingTest, $upcomingTest
+                ], "pastTest" => [
+                    $pastTest, $pastTest
+                ]]);
+
+
+                ?>
+            </p>
+        </div>
+    </div>
+</div>
+<div class="columns">
+    <div class="column is-offset-5 is-two-fifths is-warning">
+        <form method="post" action="/delete">
+            <div class="form-group">
+                <input type="submit" href="/delete" class="button is-danger is-6" value="!!! DELETE ACCOUNT !!!">
+            </div>
+        </form>
+    </div>
+</div>
+
+
+<?php
+
+
+/*
+
+
+    echo $engine->render("diagnosis",
+        ["diagnosis" => [
+            ["date" => "date 1"],
+            ["date" => "date 2"]
+        ]]);
+}
+
+*/
+?>
+<!--
+<form method='post' action="./delete">
+    <div class="form-group">
+        <input type="submit" href="/delete" class="button button_delete" value="!!! DELETE ACCOUNT !!!">
+    </div>
+</form>
 </div>
 <div class="">
-    <div class="centerdiv">
-        <br><br>
-        <h2>Some Titles/Tables in the Main Menu</h2>
+<div class="centerdiv">
+    <br><br>
+    <h2>Some Titles/Tables in the Main Menu</h2>
 
-        <br>
-    </div>
+    <br>
+</div>
 
-
-    <form method="post" action="./profile">
-        <div class="form-group">
-            <input type="submit" class="button button_submit" value="Go to Profile Page">
-        </div>
-    </form>
 
 </div>
+-->
 </body>
 </html>
 
--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?php
-require_once(__DIR__."config.php");
+require_once(__DIR__ . "/config.php");
 startDefaultSessionWith();
 
 // Clear all the session variables
