f8e0e72e7074d8944d8e6509a4bc9a84f010a66b
Giray Akyol <giray.akyol@ug.bilkent.edu.tr>
2021-12-24 19:50:11 +0300

    nits

--- login/index.php
+++ login/index.php
@@ -8,7 +8,7 @@ require_once(rootDirectory() . "/util/UserFactory.php");
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>{{title}}</title>
+    <title>Log In</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
     <style>
@@ -56,10 +56,16 @@ if (isset($conn) && $_SERVER["REQUEST_METHOD"] == "POST") {
         else if (true) // password is wrong
         {
             echo $m->render('login', [
-                'title' => 'Login',
+                'title' => 'Log in',
                 'id' => 'University ID',
                 'pass' => 'Password',
                 'passErr' => 'Password is wrong'
+            ]);
+        } else {
+            echo $m->render('login', [
+                'title' => 'Login',
+                'id' => 'University ID',
+                'pass' => 'Password'
             ]);
         }
     }
