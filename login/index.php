c0a8a37ed0dfb95c2b988cf964d3caae9793faed
Giray Akyol <giray.akyol@ug.bilkent.edu.tr>
2021-12-24 18:39:27 +0300

    Added error feedback for login page

--- login/index.php
+++ login/index.php
@@ -1,21 +1,37 @@
 <?php
-require_once( __DIR__."/../config.php");
-require_once( rootDirectory() . '/vendor/autoload.php');
-require_once( rootDirectory() . "/util/UserFactory.php");
+require_once(__DIR__ . "/../config.php");
+require_once(rootDirectory() . '/vendor/autoload.php');
+require_once(rootDirectory() . "/util/UserFactory.php");
+?>
+<!DOCTYPE html>
 
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>{{title}}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    <style>
+
+    </style>
+</head>
+<body>
+<?php
 $m = new Mustache_Engine(array(
     'loader' => new Mustache_Loader_FilesystemLoader(rootDirectory() . '/templates'),
 ));
-echo $m->render('login', array('title' => 'Login', 'id' => 'University ID', 'pass' => 'Password'));
+
 
 startDefaultSessionWith();
 $conn = getDatabaseConnection();
+
 
 if (isset($conn) && $_SERVER["REQUEST_METHOD"] == "POST") {
     // userid and password sent from the form
     $userid = $_POST['userid'];
     $password = $_POST['password'];
     $usertype = $_POST['usertype'] ?? Student::TABLE_NAME;
+
 
     try {
         //$std = new Student($conn, $userid, $password);
@@ -30,6 +46,30 @@ if (isset($conn) && $_SERVER["REQUEST_METHOD"] == "POST") {
         header("location: .."); //redirect to main page
 
     } catch (Exception $e) {
-        echo "<script type='text / javascript'>alert('" . $e->getMessage() . $_POST['password'] . "');</script>";
+        if (true) // id is wrong
+            echo $m->render('login', [
+                'title' => 'Login',
+                'id' => 'University ID',
+                'pass' => 'Password',
+                'idErr' => 'University ID is wrong'
+            ]);
+        else if (true) // password is wrong
+        {
+            echo $m->render('login', [
+                'title' => 'Login',
+                'id' => 'University ID',
+                'pass' => 'Password',
+                'passErr' => 'Password is wrong'
+            ]);
+        }
     }
+} else {
+    echo $m->render('login', [
+        'title' => 'Login',
+        'id' => 'University ID',
+        'pass' => 'Password'
+    ]);
 }
+?>
+</body>
+</html>
