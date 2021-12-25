92df635db050bac2693382194cc1228f011122ed
MUHAMMED CAN KÜÇÜKASLAN <muhammedcankucukaslan@gmail.com>
2021-12-26 02:29:33 +0300

    Sample config file added.

---
+++ sample_constants.php
@@ -0,0 +1,11 @@
+<?php
+// TODO: database connection credentials
+// Rename this file to constants.php
+// The given credentials are default settings of the MySql
+// However You shouldn't use these default values,
+// and you should set a password and/or create new user etc.
+define('password', ''); // replace empty string with your password
+define('servername', 'localhost'); // replace 'localhost' with your server name (database)
+define('username', 'root'); // replace 'root' with the username of account that you gave R/W access to the corresponding database!
+define('dbname', 'example'); // replace 'example' with your database name
+?>
